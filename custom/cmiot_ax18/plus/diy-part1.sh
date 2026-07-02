fbb2cfb98078d352d19efdc313c5e0a6c956a255
yeyaozhou <a13you@gmail.com>
2026-07-02 23:34:14 +0800

    fix: comment out broken telephony feed (freeswitch/kamailio Makefile errors)

--- custom/cmiot_ax18/plus/diy-part1.sh
+++ custom/cmiot_ax18/plus/diy-part1.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3ter.x.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
+# https://github.com/P3TERX/Actions-OpenWRT
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Description: OpenWRT DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Remove broken telephony feed (friscale/freeswitch and kamailio Makefile errors)
+sed -i '/^src-git \/https://github.com/openwrt/telephony\.git/',//&# \kermised\kermised/' feeds.conf.default
